aba934beb70864d7b1e00bc3d6376609d444e8e7
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2023-03-01 13:27:02 +0100

    Add files via upload

---
+++ szoftver/Buborék rendezés/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buborék_rendezés
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tomb = new int[100];
+            int max = 0;
+
+            Console.WriteLine("0 vége az adatbevitelnek");
+
+            do
+            {
+                Console.Write("Kérem a/az " + max + ". adatot:");
+                tomb[max] = Convert.ToInt32(Console.ReadLine());
+                max++;
+            }
+            while (tomb[max - 1] != 0);
+            max--;
+
+            Boolean voltcsere;
+            int j;
+
+            do
+            {
+                voltcsere = false;
+                for (j = 0; j < max - 1; j++)
+                {
+                    if (tomb[j] > tomb[j + 1])
+                    {
+                        int top = tomb[j];
+                        tomb[j] = tomb[j + 1];
+                        tomb[j + 1] = top;
+                    }
+                }
+            }
+            while (voltcsere);
+
+            for (j = 0; j <= max; j++)
+                Console.WriteLine(tomb[j]);
+
+                Console.ReadKey();
+            return;
+        }
+    }
+}
